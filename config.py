5c8e342f3048338462ae0557da9e63c25d2a2c33
yl1183 <b.r.u.c.e@aol.com>
2024-02-13 22:27:34 -0800

    change config ignore

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-data_base_path = ''
+connection_name = 'braided-analyst-411321:us-central1:laformula'
+database_host = '34.134.200.138'
+circuits_api = 'http://ergast.com/api/f1/circuits.json'
+database_user = 'sqlserver'
+database_password = 'sqlserver'
+database_name = 'laformula'
